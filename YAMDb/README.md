48db554e0b42979e8461cc8befbcbbcf6edf10e0
anay2103 <yana-karausheva@yandex.ru>
2021-09-25 11:48:17 +0300

    after refactoring

--- YAMDb/README.md
+++
@@ -1,3 +0,0 @@
-# yamdb_final
-yamdb_final
-![yamdb_workflow](https://github.com/anay2103/yamdb_final/actions/workflows/yamdb_workflow.yml/badge.svg)
---
+++ api/urls.py
@@ -0,0 +1,32 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import (CategoryViewSet, CommentViewSet, GenreViewSet,
+                    MyTokenObtainView, ReviewViewSet, SendEmailView,
+                    TitlesViewSet, UserViewSet)
+
+router_v1 = DefaultRouter()
+router_v1.register('users', UserViewSet, basename='User')
+
+router_v1.register('titles', TitlesViewSet, basename='Title')
+router_v1.register('genres', GenreViewSet, basename='Genre')
+router_v1.register('categories', CategoryViewSet, basename='Category')
+router_v1.register(
+    r'titles/(?P<title_id>[0-9]+)/reviews', ReviewViewSet, basename='Review'
+)
+router_v1.register(
+    r'titles/(?P<title_id>[0-9]+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet, basename='Comment'
+)
+
+auth_urls = [
+    path('email/', SendEmailView.as_view(),
+         name='obtain_confirmation_code'),
+    path('token/', MyTokenObtainView.as_view(),
+         name='obtain_token'),
+]
+
+urlpatterns = [
+    path('v1/', include(router_v1.urls)),
+    path('v1/auth/', include(auth_urls)),
+]
